f85eab83930d8e2632ae09e9a15fd4eeaa641f69
KarinaBertosh <cherrycake246@gmail.com>
2024-03-28 11:13:43 +0300

    feat: configure routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
 import { Main } from './pages/Main';
+import { Routes, Route, Outlet, Link, Router } from "react-router-dom";
+import { CoinTable } from './components/CoinTable/CoinTable';
+import { SearchInput } from './components/SearchInput/SearchInput';
+import { Info } from './components/Info/Info';
 
-export const App = () =>  {
+
+export const App = () => {
     return (
-        <Main />
+        <div className="App">
+          <Routes>
+            <Route path="*" element={<Main />} />
+            <Route path='/' element={<Main />} />
+            <Route path='/info' element={<Info />} />
+          </Routes>
+        {/* <Main /> */}
+      </div>
     );
-}
+};

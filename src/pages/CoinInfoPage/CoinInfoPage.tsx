71b4bf3ad830b19e8cb63de36d2e1f92289ef191
KarinaBertosh <cherrycake246@gmail.com>
2024-04-16 11:36:38 +0300

    refactor code

--- src/pages/CoinInfoPage/CoinInfoPage.tsx
+++ src/pages/CoinInfoPage/CoinInfoPage.tsx
@@ -23,8 +23,6 @@ export const CoinInfoPage = () => {
         id: selectedCoin.id,
         interval: chartType
       }));
-      console.log({coinPrices});
-      
       setCoinPrices(coinPrices.map((el: ICoinRow) => getFormattedValue(el.priceUsd, 0)));
       setIsLoading(false);
     })();
